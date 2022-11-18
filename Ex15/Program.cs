70e67ec1f761664f084194e2d4a71a4c52c42734
Roman Borisov <boriso-rom@yandex.ru>
2022-11-18 19:49:17 +0300

    added ex15 code

---
+++ Ex15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите день недели: ");
+string userInput = Console.ReadLine() ?? "";
+int weekDay = int.Parse(userInput);
+if(weekDay == 6 || weekDay == 7)
+{
+    Console.WriteLine($"Результат: {weekDay} -> да");
+}
+else
+{
+    Console.WriteLine($"Результат: {weekDay} -> нет");
+}
