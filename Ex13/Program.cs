b4ab8d70884ff309c3b466964e10de992b774bcb
Roman Borisov <boriso-rom@yandex.ru>
2022-11-18 18:40:23 +0300

    added ex13 code

---
+++ Ex13/Program.cs
@@ -0,0 +1,19 @@
+﻿int number;
+Console.WriteLine("Введите трехзначное число: ");
+while(true)
+{
+    if(int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine($"Результат: Ошибка ввода!");
+}
+int c = number / 100;
+if (c < 1)
+{
+    Console.WriteLine($"Результат: {number} -> Третьей цифры нет");
+}
+else
+{
+    int a = number % 100;
+    int b = a % 10;
+    Console.WriteLine($"Результат: {number} -> {b}");
+}
