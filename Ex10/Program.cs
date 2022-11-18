729356f78e509b2e1e82dca611fbfe16e30dea6e
Roman Borisov <boriso-rom@yandex.ru>
2022-11-18 18:19:48 +0300

    first commit

---
+++ Ex10/Program.cs
@@ -0,0 +1,11 @@
+﻿int number;
+while(true)
+{
+    Console.Write("Введите трёхзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+int a = number % 100;
+int b = a / 10;
+Console.WriteLine($"Результат: {number} -> {b}");
